4cb049cf9c9a5b9e9785e8ecc416b5cef6905a2d
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2024-04-17 09:18:26 +0100

    Create weather.py
    Python file about weather

---
+++ weather.py
@@ -0,0 +1,14 @@
+weather = input("What is the weather like today? \n")
+
+if weather == "sunshine" or "sunny":
+    print("You should go outside!")
+elif weather == "rain" or "raining":
+    print("You should stay inside!")
+elif weather == "snow" or "snowing":
+    print("You should go sledding!")
+elif weather =="cloudy":
+    print("You should go to the beach!")
+elif weather == "windy":
+    print("You should fly a kite!")
+else:
+    print("I don't know what to do!")
